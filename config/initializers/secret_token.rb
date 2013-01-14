1a0eeb81883885856d5f97408e525c708e34e259
Carolina <carolgarcia@dit.upm.es>
2013-01-14 14:50:20 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartCityGuide::Application.config.secret_token = 'adb0850b095e72d5f931c0c7e9bc1b64f8b4a5f989be0d2b1105dfd24df95eb44395aa48b970d1be75b806d3c0d93a6dc77a654d3211f79bab41acfc3d83b137'
