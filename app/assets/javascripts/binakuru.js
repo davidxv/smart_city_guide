78a7dc3057a39fbc7729ce9596b6ecde76c9f196
Juan Carlos <juanka0087@hotmail.com>
2013-03-13 15:54:42 +0100

    trips

---
+++ app/assets/javascripts/binakuru.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+    function showContent() {
+        element = document.getElementById("contento");
+        check = document.getElementById("check");
+        if (check.checked) {
+            element.style.display='block';
+        }
+        else {
+            element.style.display='none';
+        }
+    }
+</script>
