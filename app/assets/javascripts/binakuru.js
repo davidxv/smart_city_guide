e3b359e25287bddb5f31412d37d8dde20c6a945d
Juan Carlos <juanka0087@hotmail.com>
2013-03-15 17:10:47 +0100

    trip 2

--- app/assets/javascripts/binakuru.js
+++ app/assets/javascripts/binakuru.js
@@ -1,12 +1,50 @@
-<script type="text/javascript">
-    function showContent() {
-        element = document.getElementById("contento");
-        check = document.getElementById("check");
-        if (check.checked) {
-            element.style.display='block';
-        }
-        else {
-            element.style.display='none';
-        }
-    }
-</script>
+
+function showContentGuides() {
+  element = document.getElementById("content_guides");
+  check = document.getElementById("checkGuides");
+  if (check.checked) {
+    element.style.display='block';
+  }
+  else {
+    element.style.display='none';
+  }
+
+}
+  function displayResult()
+  {
+    var x=document.getElementById("checkGuides").defaultChecked;
+    alert(x);
+  }
+
+function showContentRoutes() {
+  element = document.getElementById("content_routes");
+  check = document.getElementById("checkRoutes").defaultChecked;
+  if (check.checked) {
+    element.style.display='block';
+  }
+  else {
+    element.style.display='none';
+  }
+}
+
+function showContentPoints() {
+  element = document.getElementById("content_points");
+  check = document.getElementById("checkPoints").defaultChecked;
+  if (check.checked) {
+    element.style.display='block';
+  }
+  else {
+    element.style.display='none';
+  }
+}
+
+
+$(function() {
+  $( ".button-resizable" ).click(function(){
+    $( ".minimized" ).switchClass( "minimized", "expanded", 1000 );
+    $( ".expanded" ).switchClass( "expanded", "minimized", 1000 );
+    return false;
+  });
+});
+
+var x=document.getElementById("myCheck").defaultChecked;
