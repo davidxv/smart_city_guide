8c9f9012192e2bfde7427d37328953424e19be98
Carolina <carolgarcia@dit.upm.es>
2013-04-16 15:32:26 +0200

    Add places photos to routes and guides

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,4 +3,12 @@ class Route < ActiveRecord::Base
   belongs_to :trip
   belongs_to :guide
   has_and_belongs_to_many :places
+
+  def photos
+    p = Set.new
+    places.each do |a|
+      p << a.photos
+    end
+    p
+  end
 end
