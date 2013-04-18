febc216ab63c40100dfa5d43197d21780510afe4
Juan Carlos <juanka0087@hotmail.com>
2013-04-18 16:39:01 +0200

    layout

--- config/application.rb
+++ config/application.rb
@@ -65,8 +65,6 @@ module SmartCityGuide
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.to_prepare do
-      UsersController.layout "users"
-    end
+
   end
 end
